27dc28566eb1c0666f35f661b35d32395833a01b
Dominika Dziasek <ddziasek45@gmail.com>
2022-12-20 17:47:29 +0100

    add sass

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${inputSearch}`)
     <div class="section-show-box__dish" >
         <img src="${elem.strMealThumb}" alt="food-image" class = "section-show-box__image">
         <h1 class = "section-show-box__title">${elem.strMeal}</h1>
-        <button class="section-show-box__recipe buttons" data-id = ${elem.idMeal}>Get recipe</button>
+        <button class="section-show-box__recipe  buttons" data-id = ${elem.idMeal}>Get recipe</button>
     </div>
 `
 })
@@ -30,6 +30,7 @@ fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${inputSearch}`)
 
 // Show more details about dish(recipe)
 function getMealRecipe(e) {
+
   if(e.target.classList.contains('section-show-box__recipe')){
     let getID = Number(e.target.dataset.id);
     let arrIngredient = []
@@ -38,6 +39,7 @@ fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${getID}`)
     .then(response => response.json())
     .then(data => { 
         let getDetails = data.meals[0]
+        console.log(getDetails)
         arrIngredient.push(getDetails.strIngredient1, getDetails.strIngredient2, getDetails.strIngredient3, getDetails.strIngredient4, getDetails.strIngredient5, getDetails.strIngredient6, getDetails.strIngredient7, getDetails.strIngredient8, getDetails.strIngredient9, getDetails.strIngredient10, getDetails.strIngredient11, getDetails.strIngredient12, getDetails.strIngredient13, getDetails.strIngredient14, getDetails.strIngredient15, getDetails.strIngredient16, getDetails.strIngredient17, getDetails.strIngredient18, getDetails.strIngredient19, getDetails.strIngredient20);
 
 let filteredArr = arrIngredient.filter(elem => elem !== "")
