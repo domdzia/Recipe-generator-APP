4eb90bfde35f1c85914a2b9c1731f2473685d0d9
Dominika Dziasek <ddziasek45@gmail.com>
2023-03-24 10:44:15 +0100

    change modal function

--- main.js
+++ main.js
@@ -1,50 +1,74 @@
-const btnSearch = document.querySelector('.main__btn-search');
-const sectionListRecipe = document.querySelector('.section-show-box');
-const recipeBox = document.querySelector('.recipe-box')
+const btnSearch = document.querySelector(".main__btn-search");
+const sectionListRecipe = document.querySelector(".section-show-box");
+const recipeBox = document.querySelector(".recipe-box");
+const suggestionTitle = document.querySelector(".suggestion-text");
+let inputSearch = document.querySelector(".main__input-search");
 
 // Create list of dishes
 
-function searchRecipe (e) {
-sectionListRecipe.innerHTML = "";
-let inputSearch = document.querySelector('.main__input-search').value;
+function searchRecipe(e) {
+  sectionListRecipe.innerHTML = "";
+  inputSearch = document.querySelector(".main__input-search").value;
 
-fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${inputSearch}`)
-.then(response => response.json())
-.then(data => { 
-    if(!data.meals){
-        sectionListRecipe.innerHTML = `Sorry, we didn't find recipe for you.`
-    }
-	data.meals.forEach(elem => { 
-		sectionListRecipe.innerHTML += `
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${inputSearch}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (!data.meals) {
+        sectionListRecipe.innerHTML = `Sorry, we didn't find recipe for you.`;
+      }
+      if (data.meals) {
+        suggestionTitle.innerHTML = "Here are suggestions for you:";
+      }
+      data.meals.forEach((elem) => {
+        sectionListRecipe.innerHTML += `
     <div class="section-show-box__dish" >
         <img src="${elem.strMealThumb}" alt="food-image" class = "section-show-box__image">
         <h1 class = "section-show-box__title">${elem.strMeal}</h1>
         <button class="section-show-box__recipe  buttons" data-id = ${elem.idMeal}>Get recipe</button>
     </div>
-`
-})
-})
-.catch((err) => err)
+`;
+      });
+    })
+    .catch((err) => err);
 }
-
 
 // Show more details about dish(recipe)
 function getMealRecipe(e) {
+  let getID = Number(e.target.dataset.id);
+  let arrIngredient = [];
 
-  if(e.target.classList.contains('section-show-box__recipe')){
-    let getID = Number(e.target.dataset.id);
-    let arrIngredient = []
-    
-fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${getID}`)
-    .then(response => response.json())
-    .then(data => { 
-        let getDetails = data.meals[0]
-        console.log(getDetails)
-        arrIngredient.push(getDetails.strIngredient1, getDetails.strIngredient2, getDetails.strIngredient3, getDetails.strIngredient4, getDetails.strIngredient5, getDetails.strIngredient6, getDetails.strIngredient7, getDetails.strIngredient8, getDetails.strIngredient9, getDetails.strIngredient10, getDetails.strIngredient11, getDetails.strIngredient12, getDetails.strIngredient13, getDetails.strIngredient14, getDetails.strIngredient15, getDetails.strIngredient16, getDetails.strIngredient17, getDetails.strIngredient18, getDetails.strIngredient19, getDetails.strIngredient20);
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${getID}`)
+    .then((response) => response.json())
+    .then((data) => {
+      let getDetails = data.meals[0];
+      arrIngredient.push(
+        getDetails.strIngredient1,
+        getDetails.strIngredient2,
+        getDetails.strIngredient3,
+        getDetails.strIngredient4,
+        getDetails.strIngredient5,
+        getDetails.strIngredient6,
+        getDetails.strIngredient7,
+        getDetails.strIngredient8,
+        getDetails.strIngredient9,
+        getDetails.strIngredient10,
+        getDetails.strIngredient11,
+        getDetails.strIngredient12,
+        getDetails.strIngredient13,
+        getDetails.strIngredient14,
+        getDetails.strIngredient15,
+        getDetails.strIngredient16,
+        getDetails.strIngredient17,
+        getDetails.strIngredient18,
+        getDetails.strIngredient19,
+        getDetails.strIngredient20
+      );
 
-let filteredArr = arrIngredient.filter(elem => elem !== "")
+      let filteredArr = arrIngredient.filter((elem) => elem !== "");
 
-        recipeBox.innerHTML = `
+      console.log(filteredArr);
+
+      recipeBox.innerHTML = `
         <img class="recipe-box__smaller-pic" src="${getDetails.strMealThumb}" alt="small-food-image">
         <h3 class = "recipe-box__title">${getDetails.strMeal}</h3>
         <div class = "recipe-box__text"> You need: </div>
@@ -53,27 +77,28 @@ let filteredArr = arrIngredient.filter(elem => elem !== "")
         </ul>
         <button class="recipe-box__close-button">X</button>
         <p class = "recipe-box__instructions">${getDetails.strInstructions}</p> 
-        `
-recipeBox.classList.add('show');
-       recipeBox.querySelector('button').addEventListener("click", hideBox)
+        `;
+      recipeBox.classList.add("show");
+      recipeBox.querySelector("button").addEventListener("click", hideBox);
     })
-    .catch((err) => err)
-
-    }
- 
-  }
-
-
-  // Hidden the recipe box
-function hideBox() {
-    recipeBox.classList.remove('show')
+    .catch((err) => err);
 }
 
+// Hidden the recipe box
+function hideBox() {
+  recipeBox.classList.remove("show");
+}
 
-// Search recipes
+// Search recipes on Click mouse
 btnSearch.addEventListener("click", searchRecipe);
 
+// Search recipes on Click enter
+inputSearch.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    searchRecipe();
+  }
+});
 
-// Show details 
+// Show details
 
-sectionListRecipe.addEventListener('click', getMealRecipe);
+sectionListRecipe.addEventListener("click", getMealRecipe);
